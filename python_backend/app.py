f6b2545706d61eaaae969ef65e8d79386bfa0c54
SIRUVELLA ABHINAYA <245123733001@mvsrec.edu.in>
2026-03-06 16:08:37 +0530

    Update app.py

--- python_backend/app.py
+++ python_backend/app.py
@@ -1293,5 +1293,5 @@ def get_shocks():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
